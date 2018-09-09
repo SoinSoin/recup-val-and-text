2b4fac7940095fec20d8f5365f31145c7c282752
SoinSoin <vincent.retore@gmail.com>
2018-09-09 10:36:18 +0200

    petit test js

---
+++ app.js
@@ -0,0 +1,42 @@
+class Element {
+    constructor(tag) {
+        this.tag = tag;
+    }
+    get txt() {
+        return this.SetTxt();
+    }
+    SetTxt() {
+        try {
+            let thisTxt = document.querySelectorAll(this.tag);
+            let arr = [];
+            thisTxt.forEach(element => {
+                arr.push(element.textContent);
+            })
+            return arr;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    get val() {
+        return this.SetVal();
+    }
+    SetVal() {
+        try {
+            let thisTxt = document.querySelectorAll(this.tag);
+            let arr = [];
+            thisTxt.forEach(element => {
+                arr.push(element.value);
+            })
+            return arr;
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+document.getElementById('click').onclick = recupvalue;
+
+function recupvalue() {
+    var test = new Element('.myText');
+    console.log(test.val)
+}
